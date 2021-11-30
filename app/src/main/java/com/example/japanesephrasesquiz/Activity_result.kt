7f3866b590bb19a3476c9039727f0f9ae02eeff5
Sebastian Correa <correa.sebas6@gmail.com>
2021-11-30 18:19:35 -0500

    Preparing the UI for the result activity

---
+++ app/src/main/java/com/example/japanesephrasesquiz/Activity_result.kt
@@ -0,0 +1,11 @@
+package com.example.japanesephrasesquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
